9b79bcc5bff59d921d88c55dac5c9243999c62bf
pudge <haider@isometrylabs.com>
2016-01-03 16:03:28 +0500

    first commit

---
+++ fizz_buzz.rb
@@ -0,0 +1,13 @@
+class FizzBuzz
+
+# ruby -r "./fizz_buzz.rb" -e "FizzBuzz.fizz_buzz" to run from command line
+	def self.fizz_buzz
+		(1..100).each do |num|
+			text = ''
+		    text += 'Fizz' if num%3 == 0
+		    text += 'Buzz' if num%5 == 0
+		    puts text.size == 0 ? num : text
+		end
+	end
+
+end
---
+++ test_fizz_buzz.rb
@@ -0,0 +1,38 @@
+require 'simplecov'
+SimpleCov.start
+
+require_relative "fizz_buzz"
+require "test/unit"
+require 'stringio'
+
+class TestFizzBuzz < Test::Unit::TestCase
+
+	def setup
+		@output = StringIO.new()
+	    $stdout = @output
+	    FizzBuzz.fizz_buzz()
+	    @output.rewind
+	    @lines = @output.readlines.map {|str| str.chop}
+	    $stdout = STDOUT
+	    puts @lines[0]
+	end
+
+
+	test "should print 1 as the first line" do
+		assert @lines[0].to_i == 1
+	end
+
+	test "should print Fizz as the thrid line" do
+		assert @lines[2] == "Fizz"
+	end
+
+	test "should print Buzz as the fifth line" do
+		assert @lines[4]== "Buzz"
+	end
+
+	test "should print FizzBuzz as the fifteenth line" do
+		assert @lines[14]== "FizzBuzz"
+	end
+
+end
+
